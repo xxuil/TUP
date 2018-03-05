62b9a6c445194f9f2aae248c2a0e735330b55ce6
kyc375 <ckravis@utexas.edu>
2018-03-04 23:00:35 -0600

    BookStorage.java

--- src/BookStorage.java
+++ src/BookStorage.java
@@ -1,23 +1,59 @@
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class BookStorage {
     private Map<String, Integer> inventory;
-    private Map<String, String> students;
+    private ArrayList<Students> students;
+    private int count = 0;
     public BookStorage(Map<String, Integer> inventory){
         this.inventory = inventory;
-        students = new HashMap<>();
+        students = new ArrayList<>();
     }
-    public synchronized void borrow(String stdname, String bookname){
-
+    public synchronized int borrow(String stdname, String bookname){
+        int numberleft = inventory.get(bookname);
+        if(numberleft == 0){
+            return -1;
+        }else if(inventory.get(bookname) == null){
+            return -2;
+        }else{
+            count++;
+            Map<Integer, String> good = new HashMap<>();
+            good.put(count, bookname);
+            Students student = new Students(stdname, good);
+            students.add(student);
+            inventory.put(bookname, numberleft - 1);
+        }
+        return count;
     }
-    public synchronized void return_1(int i){
-
+    public synchronized boolean return_1(int input){
+        for(int i = 0; i < students.size(); i++){
+            Students allstu = students.get(i);
+            Map<Integer, String> result = allstu.bookID;
+            String returnval = result.get(input);
+            if(result.get(input) != null){
+                int a = inventory.get(returnval);
+                inventory.put(returnval, a + 1);
+                return true;
+            }
+        }
+        return false;
     }
-    public synchronized void list(String stdname){
-
+    public synchronized Map<Integer, String> list(String stdname){
+        for(int i = 0; i < students.size(); i++) {
+            Students temp = students.get(i);
+            if (temp.name.equals(stdname)) {
+                Map<Integer, String> result = temp.bookID;
+                return result;
+            }
+        }
+        return null;
     }
-    public synchronized void inventory(){
-
+    public synchronized Map<String, Integer> inventory(){
+        return this.inventory;
     }
 }

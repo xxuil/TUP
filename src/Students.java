4762b446596d64adf360e5f2cfd5ba76b3baacbb
kyc375 <ckravis@utexas.edu>
2018-03-04 22:14:47 -0600

    BookStorage.java

---
+++ src/Students.java
@@ -0,0 +1,10 @@
+import java.util.Map;
+
+public class Students {
+    public String name;
+    public Map<Integer, String> bookID;
+    public Students(String name, Map<Integer, String> bookID){
+        this.name = name;
+        this.bookID = bookID;
+    }
+}

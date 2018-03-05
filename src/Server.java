9476892a9b5d2af3569ed7d01f0bd8744d06e15b
xxuil <liuxxggwp@utexas.edu>
2018-03-04 20:41:38 -0600

    2

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,19 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class Server {
+    private final static boolean DEBUG = true;
+
+    public static void main(String[] args) throws IOException{
+        byte[] buf = new byte[1024];
+        DatagramSocket socket = new DatagramSocket(3000);
+        DatagramPacket dp_receive = new DatagramPacket(buf, 1024);
+
+        if(DEBUG){System.out.print("Server is on");}
+
+        while(true){
+            
+        }
+    }
 }
